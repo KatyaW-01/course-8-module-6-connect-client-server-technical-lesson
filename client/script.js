bd2b5010b65bdf86f7bb9dc53922e86d8357f2b0
Katya Weicht <katya.weicht@gmail.com>
2025-07-16 22:26:56 -0600

    Update scipt file

--- client/script.js
+++ client/script.js
@@ -3,3 +3,26 @@ document.getElementById('fetch-btn').addEventListener('click', async () => {
   const data = await response.json()
   document.getElementById('output').textContent = JSON.stringify(data, null, 2)
 })
+
+fetch("/products")
+  .then(response => response.json())
+  .then(data => {
+    //render products to the page
+  })
+
+// make sure you have a form in your HTML file
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const name = document.querySelector("#name").ariaValueMax;
+  const category = document.querySelector("#category").ariaValueMax;
+
+  fetch("/products", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ name, category })
+  })
+  .then(response => response.json())
+  .then(product => {
+    // update DOM with the new product
+  })
+})
